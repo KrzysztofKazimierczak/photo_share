fd6ef4e414a44f680ee72752899cfccde22ba43f
CamilleusRex <camilleusrex@gmail.com>
2024-02-28 20:51:43 +0100

    added basic search functions

---
+++ src/routes/search.py
@@ -0,0 +1,17 @@
+from sqlalchemy import or_
+
+#Mother Function
+def search():
+    pass
+
+def search_pictures():
+    pass
+
+def search_users():
+    pass
+
+def search_comments():
+    pass
+
+
+
