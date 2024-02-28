6e0f177f5a1fcc203e0abf1762482a43755ec7d5
CamilleusRex <camilleusrex@gmail.com>
2024-02-28 21:02:47 +0100

    expanded code
    added more imports
    expanded functions by arguments they take

--- src/routes/search.py
+++ src/routes/search.py
@@ -1,16 +1,20 @@
 from sqlalchemy import or_
+from src.database.models import Base, Tag, User, Comment, Picture
+from typing import List
+from datetime import datetime
+
 
 #Mother Function
-def search():
+def search(query: str, model: Base, fields: List[str]) -> List[Base]: # type: ignore
     pass
 
-def search_pictures():
+def search_pictures(keywords_or_tags: List[str]) -> List[Picture]:
     pass
 
-def search_users():
+def search_users(keywords: List[str]) -> List[User]:
     pass
 
-def search_comments():
+def search_comments(keywords: List[str]) -> List[Comment]:
     pass
 
 
